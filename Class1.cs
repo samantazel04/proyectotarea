51d610282803571698af73e96057a00cf20402a0
paulb <paulb@PAULBLANDON31>
2023-09-04 16:35:04 -0600

    cambios

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectotarea
+{
+    public class Denisse
+    {
+        public void reporte()
+        {
+            Console.WriteLine("Todo esta funcionando bien profe");
+        }
+    }
+}
